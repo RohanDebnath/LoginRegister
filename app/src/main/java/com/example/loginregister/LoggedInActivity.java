bbcc2ba033d9d1bc0f96ea73752bb8d483044f9d
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-06-29 02:38:22 +0530

    All set with name

--- app/src/main/java/com/example/loginregister/LoggedInActivity.java
+++ app/src/main/java/com/example/loginregister/LoggedInActivity.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoggedInActivity extends AppCompatActivity {
-    private Button logoutButton;
     private FirebaseAuth mAuth;
+    private DatabaseReference usersRef;
+    private TextView welcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +28,15 @@ public class LoggedInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_logged_in);
 
         mAuth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance().getReference("users");
+        welcomeTextView = findViewById(R.id.welcomeTextView);
 
-        logoutButton = findViewById(R.id.logoutButton);
-
+        Button logoutButton = findViewById(R.id.logoutBtn);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Perform logout
-                mAuth.signOut();
+                // Log out the user
+                FirebaseAuth.getInstance().signOut();
 
                 // Redirect to the login activity
                 Intent intent = new Intent(LoggedInActivity.this, MainActivity.class);
@@ -34,5 +44,26 @@ public class LoggedInActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            usersRef.child(userId).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String name = snapshot.getValue(String.class);
+                    if (name != null) {
+                        String welcomeMessage = "Welcome, " + name;
+                        welcomeTextView.setText(welcomeMessage);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // Handle the error
+                }
+            });
+        }
     }
 }
